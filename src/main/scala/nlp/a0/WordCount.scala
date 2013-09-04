93593eeafadddcd1189c0e295ae696c4c70ba094
Sam Acuna <samueleishion@gmail.com>
2013-09-04 10:45:19 -0500

    Made WordCount more efficient by reducing lines of code

--- src/main/scala/nlp/a0/WordCount.scala
+++ src/main/scala/nlp/a0/WordCount.scala
@@ -1,125 +1,60 @@
 package nlp.a0
 
-import scala.io.Source
-import scala.collection.mutable.Map
-import scala.collection.immutable.ListMap
-import scala.collection.immutable.IndexedSeq
+import nlp.a0.Process
 
 object WordCount {
-  def main(args: Array[String]) {
+  def main(args:Array[String]) {
     if(args(0)==null) {
-      println("Please enter a proper file.") 
       throw new Exception("Please enter a proper file.")
     } else {
       var command = "default"
-      
+
       if(args.length >= 2 && args(1)!=null) {
         command = args(1)
-      } 
+      }
 
       command match {
-        case "--stopwords" => 
-          if(args(2)==null) {
-            println("Please enter a file with stopwords.")
+        case "--stopwords" =>
+          if(args(2)==null) 
             throw new Exception("Please enter a file with stopwords.")
-          } else { 
-            var bookStr = textToString(args(0))
-            var stopStr = textToString(args(2))
-            var bookWords = filterText(bookStr)
-            var stopWords = filterText(stopStr)
-            println("Total number of words: "+bookWords.length.toString)
-
-            var bookFreqs = countUniqueFrequenciesWithStop(bookWords,stopWords)
-            println("Number of distinct words: "+bookFreqs.length.toString)
-            println("Top 10 words: ")
-            var i = 0
-            for(freq <- bookFreqs) {
-              if(i < 10) {
-                var k: String = freq._1.asInstanceOf[String] 
-                var v: Int = freq._2.asInstanceOf[Int]
-                var p = (v.toFloat/bookWords.length)*100
-
-                if(k.length>7) {
-                  println("%s\t%d\t%.2f".format(k,v,p))
-                } else {
-                  println("%s\t\t%d\t%.2f".format(k,v,p))
-                }
-              }
-              i += 1 
-            }
-          }
+          else {
+            processBook(args(0),args(2))
+          } 
         case _ =>
-          var bookStr = textToString(args(0))
-          var bookWords = filterText(bookStr)
-          println("Total number of words: "+bookWords.length.toString) 
-
-          var bookFreqs = countUniqueFrequencies(bookWords) 
-          println("Number of distinct words: "+bookFreqs.length.toString)
-          println("Top 10 words: ")
-          var i = 0
-          for(freq <- bookFreqs) {
-            if(i < 10) {
-              var k: String = freq._1.asInstanceOf[String] 
-              var v: Int = freq._2.asInstanceOf[Int]
-              var p = (v.toFloat/bookWords.length)*100
-
-              println("%s\t\t%d\t%.2f".format(k,v,p))
-            }
-            i += 1 
-          }
+          processBook(args(0),"")
       }
     }
   }
 
-  def textToString(book: String) : String = {
-    var source = Source.fromFile(book) 
-    var string = source.getLines mkString "\n"
-    source.close() 
-    return string 
-  }
+  def processBook(text: String, stop: String) {
+    val bookStr = Process.textToString(text)
+    var stopStr = ""
+    if(stop!="") 
+      stopStr = Process.textToString(stop)
 
-  def filterText(text: String) : Array[String] = {
-    var string = text.replaceAll("[^A-Za-z]"," ")
-    string = string.replaceAll(" +"," ")
-    string = string.toLowerCase()
-    var words = string.split(" ") 
-    return words
-  }
+    val bookWords = Process.filterText(bookStr)
+    val stopWords = Process.filterText(stopStr)
+    println("Total number of words: "+bookWords.length.toString)
 
-  def countUniqueFrequencies(words: Array[String]) : IndexedSeq[(String,Int)] = {
-    var map:Map[String,Int] = Map()
-
+    val bookFreqs = Process.countUniqueFrequenciesWithStop(bookWords,stopWords)
+    println("Number of distinct words: "+bookFreqs.length.toString)
+    println("Top 10 words: ")
     var i = 0
-    for(i <- 0 to words.length-1) {
-      if(map.contains(words(i))) {
-        map(words(i)) += 1
-      } else {
-        map += (words(i) -> 1)
+    var k = ""
+    var v = 0
+    var p = 0.0
+    for(freq <- bookFreqs) {
+      if(i < 10) {
+        k = freq._1.asInstanceOf[String]
+        v = freq._2.asInstanceOf[Int] 
+        p = (v.toFloat/bookWords.length)*100
+
+        if(k.length>7) 
+          println("%s\t%d\t%.2f".format(k,v,p))
+        else 
+          println("%s\t\t%d\t%.2f".format(k,v,p))
       }
+      i += 1
     }
-
-    var list = ListMap(map.toList.sortBy{_._2}:_*).toIndexedSeq
-    list = list.reverse
-
-    return list 
-  }
-
-  def countUniqueFrequenciesWithStop(words: Array[String], stops: Array[String]) : IndexedSeq[(String,Int)] = {
-    var map:Map[String,Int] = Map()
-
-    var i = 0
-    for(i <- 0 to words.length-1) {
-      if(map.contains(words(i))) {
-        map(words(i)) += 1
-      } else {
-        if(!stops.contains(words(i))) {
-          map += (words(i) -> 1)
-        }
-      }
-    }
-
-    var list = ListMap(map.toList.sortBy{_._2}:_*).toIndexedSeq.reverse
-
-    return list
   }
 }
