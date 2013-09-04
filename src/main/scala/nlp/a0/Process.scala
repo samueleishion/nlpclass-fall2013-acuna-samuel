31a5363ebbd229a1fdaaabe2e5cacdd2d1eeb950
Sam Acuna <samueleishion@gmail.com>
2013-09-04 08:01:14 -0500

    Added part 4 of hw and Process object with text processing methods

---
+++ src/main/scala/nlp/a0/Process.scala
@@ -0,0 +1,102 @@
+package nlp.a0
+
+import scala.io.Source
+import scala.collection.mutable.Map
+import scala.collection.immutable.ListMap
+import scala.collection.immutable.IndexedSeq
+import scala.collection.immutable.ListMap 
+
+object Process {
+
+	def textToString(book: String) : String = {
+		val source = Source.fromFile(book) 
+		val string = source.getLines mkString "\n"
+		source.close() 
+		return string 
+	}
+
+	def filterText(text: String) : Array[String] = {
+		var string = text.replaceAll("[^A-Za-z]"," ")
+		string = string.replaceAll(" +"," ")
+		string = string.toLowerCase()
+		val words = string.split(" ") 
+		return words
+	}
+
+	def countUniqueFrequencies(words: Array[String]) : IndexedSeq[(String,Int)] = {
+		var map:Map[String,Int] = Map()
+
+		var i = 0
+		for(i <- 0 to words.length-1) {
+		  if(map.contains(words(i))) {
+		    map(words(i)) += 1
+		  } else {
+		    map += (words(i) -> 1)
+		  }
+		}
+
+		val list = ListMap(map.toList.sortBy{_._2}:_*).toIndexedSeq.reverse
+
+		return list 
+	}
+
+	def countUniqueFrequenciesWithStop(words: Array[String], stops: Array[String]) : IndexedSeq[(String,Int)] = {
+		var map:Map[String,Int] = Map()
+
+		var i = 0
+		for(i <- 0 to words.length-1) {
+		  if(map.contains(words(i))) {
+		    map(words(i)) += 1
+		  } else {
+		    if(!stops.contains(words(i))) {
+		      map += (words(i) -> 1)
+		    }
+		  }
+		}
+
+		return ListMap(map.toList.sortBy{_._2}:_*).toIndexedSeq.reverse
+	}
+
+	// def countFrequencyFrequencies(freqs: IndexedSeq[(String,Int)]) : Array[Int] = {
+		// val largest: Int = freqs.head._2.asInstanceOf[Int]
+
+		// val map:Map[Int,Int] = Map()
+
+		// var seq = ("",0)
+		// for(seq <- freqs) {
+		// 	if(map.contains(freqs._1)) {
+		// 		map(freqs._1) += 1
+		// 	}
+		// }
+
+		// return 0
+	// }
+
+	def countFrequencyFrequencies(freqs: IndexedSeq[(String,Int)]) : IndexedSeq[(Int,Int)] = {
+		val map:Map[Int,Int] = Map()
+
+		var i = 0
+		for(i <- 0 to freqs.length-1) {
+			var v = freqs(i)._2 
+			if(map.contains(v)) {
+				map(v) += 1
+			} else {
+				map += (v -> 1)
+			}
+		}
+
+		return ListMap(map.toList.sortBy{_._2}:_*).toIndexedSeq.reverse
+	}
+
+	def pluralOrSingular(singularForm: String, quantity: Int) : String = {
+		if(quantity>1) 
+			return singularForm+"s" 
+		return singularForm 
+	}
+
+	def conjugate(verb: String, given: String) : String = {
+		if(given(given.length-1)=='s') 
+			return verb
+		return verb+"s"
+	}
+}
--- src/main/scala/nlp/a0/WordFreqFreq.scala
+++ src/main/scala/nlp/a0/WordFreqFreq.scala
@@ -1,5 +1,42 @@
 package nlp.a0
 
-object WordFreqFreq {
+import nlp.a0.Process
 
+object WordFreqFreq {
+	def main(args: Array[String]) {
+		if(args(0)==null) {
+			println("Please enter a proper file.") 
+			throw new Exception("Please enter a proper file.")
+		} else {
+			val bookStr = Process.textToString(args(0))
+			val bookWords = Process.filterText(bookStr)
+
+			println("Top 10 most frequent frequencies:") 
+			val bookFreqs = Process.countUniqueFrequencies(bookWords)
+			val freqFreqs = Process.countFrequencyFrequencies(bookFreqs) 
+			var i = 0
+			for(freq <- freqFreqs) { 
+				var words = Process.pluralOrSingular("word",freq._2)
+				var times = Process.pluralOrSingular("time",freq._1)
+				var appear = Process.conjugate("appear",words)
+				if(i < 10) {
+					println("%d %s %s %d %s".format(freq._2,words,appear,freq._1,times))
+				}
+				i += 1
+			}
+
+			println("\nBottom 5 most frequent frequencies:")
+			val lim = freqFreqs.length-1
+			i = 0
+			for(freq <- freqFreqs) {
+				var words = Process.pluralOrSingular("word",freq._2)
+				var times = Process.pluralOrSingular("time",freq._1)
+				var appear = Process.conjugate("appear",words)
+				if(i >= lim-5 && i <= lim) {
+					println("%d %s %s %d %s".format(freq._2,words,appear,freq._1,times))
+				}
+				i += 1
+			}
+		}
+	}
 }
